lxde/lxappearance-obconf

commit c5ace459a16e68445f3564f03be7f2f0248b2bc4

Check if X11 before requesting X11 WM name

src/main.c
@@ -157,8 +157,12 @@ extern gboolean plugin_load(LXAppearance* app, GtkBuilder* lxappearance_builder)
     if(app->abi_version > LXAPPEARANCE_ABI_VERSION)
         return FALSE;
 
+    GdkScreen *screen = gtk_widget_get_screen(app->dlg);
+    if(!GDK_IS_X11_SCREEN(screen))
+        return FALSE;
+
     /* detect openbox */
-    const char* wm_name = gdk_x11_screen_get_window_manager_name(gtk_widget_get_screen(app->dlg));
+    const char* wm_name = gdk_x11_screen_get_window_manager_name(screen);
     if(g_strcmp0(wm_name, "Openbox"))
         return FALSE; /* don't load the plugin if openbox is not in use. */
 

commit 8b65957fa66f103acc88d690a6d9ae96567ee6ba

Fix compilation with GTK+ 2
This fixes a regression introduced in
commit c5ace459a16e68445f3564f03be7f2f0248b2bc4,
github pull request #3.

src/main.c
@@ -158,8 +158,11 @@ extern gboolean plugin_load(LXAppearance* app, GtkBuilder* lxappearance_builder)
         return FALSE;
 
     GdkScreen *screen = gtk_widget_get_screen(app->dlg);
+
+#if GTK_CHECK_VERSION(3, 0, 0)
     if(!GDK_IS_X11_SCREEN(screen))
         return FALSE;
+#endif
 
     /* detect openbox */
     const char* wm_name = gdk_x11_screen_get_window_manager_name(screen);